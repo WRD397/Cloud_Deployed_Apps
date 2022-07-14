56cb12f845fc7592ab5cb5eb27167b4318095b47
WRD397 <writd97@gmail.com>
2022-07-14 18:51:42 +0530

    app.py created, database is yet to be filled

--- Flask_works/app.py
+++ Flask_works/app.py
@@ -1,11 +1,39 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+# to initialize the app. Flask is a class and app is just a variable here
 app = Flask(__name__)
+
+# initializing SQL Alchemy. It basicaly allows us to change databases in Python
+# detailed about 'SQLALCHEMY_DATABASE_URI' : https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
+# detail about 'SQLALCHEMY_TRACK_MODIFICATIONS' =  https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
+
+# removed the temp part as we want the batabse to form here only, and then changed the name from test.db to todo.db
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# we now will define the database schema through building a class
+
+
+class Todo(db.Model):
+    Serial_no = db.Column(db.Integer, primary_key=True)
+    Todo_Title = db.Column(db.String(200), nullable=False)
+    # nullable = False means it can't be null
+    desc = db.Column(db.String(1000), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.serial_no} - {self.Title}"
+
+
+# adding a decorator 'routes'. routes basically add the endpoints, ie. below method is  basically brings you to the home page.
 
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def home_page():
+    return render_template('index2.html')
 
 # Let's create another endpoint...
 
@@ -16,4 +44,4 @@ def products():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
